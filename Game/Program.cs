47d7ffb3fb220187a88b31a16a3abd3e602e3e2c
_BrenD_ <35214019+brend32@users.noreply.github.com>
2024-10-03 21:21:41 +0300

    Console upgrade

--- Game/Program.cs
+++ Game/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System.Diagnostics;
+using FastConsole.Engine.Core;
 using Game;
 
 class Program
 {
 	public static void Main()
 	{
+		Windows.ForceUpgradeToAnsi();
+		
 		SceneManager.OpenScene(new MenuScene());
 		SceneManager.Run();
 	}
